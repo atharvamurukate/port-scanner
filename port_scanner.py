db1d40af0aac06e545d34eae9b02d08d98d77092
Atharva Murukate <murukateatharva@gmail.com>
2026-05-14 10:47:59 +0530

    Final commit

--- port_scanner.py
+++ port_scanner.py
@@ -16,5 +16,20 @@ except socket.gaierror:
 
 print(f"Scanning Target: {target_ip}")
 print(f"Time started: {datetime.now()}")
+
+start_port = int(input("Enter starting port: "))
+end_port = int(input("Enter ending port: "))
 print("\nScanning...")
+
+for port in range(start_port, end_port + 1):
+    scanner =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    scanner.settimeout(1)
+    
+    result = scanner.connect_ex((target_ip, port))
+    
+    if result == 0:
+        print(f"[OPEN] port {port}")
+        
+    scanner.close()
+print("\nScan completed.")
 
